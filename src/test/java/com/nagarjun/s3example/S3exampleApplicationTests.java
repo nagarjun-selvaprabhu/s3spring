8e8c40aa6f645d4005b72179c6970c47b5349dd8
nagarjun <selvaprabhu.nagarjun@gmail.com>
2021-08-04 11:48:38 +0530

    initial commit

---
+++ src/test/java/com/nagarjun/s3example/S3exampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarjun.s3example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3exampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
